5c37f3e560775e7f7cf9509c31a6f10e2c8f6a95
Matthew A Simonson <masimonson@gmail.com>
2017-05-21 21:19:29 -0500

    Change activation function to relu from tan

--- ImageClassifyMxnetGPU.R
+++ ImageClassifyMxnetGPU.R
@@ -64,18 +64,18 @@ data <- mx.symbol.Variable('data')
 
 # 1st convolutional layer 5x5 kernel and 20 filters.
 conv_1 <- mx.symbol.Convolution(data= data, kernel = c(5,5), num_filter = 20)
-tanh_1 <- mx.symbol.Activation(data= conv_1, act_type = "tanh")
+tanh_1 <- mx.symbol.Activation(data= conv_1, act_type = "relu")
 pool_1 <- mx.symbol.Pooling(data = tanh_1, pool_type = "max", kernel = c(2,2), stride = c(2,2))
 
 # 2nd convolutional layer 5x5 kernel and 50 filters.
 conv_2 <- mx.symbol.Convolution(data = pool_1, kernel = c(5,5), num_filter = 50)
-tanh_2 <- mx.symbol.Activation(data = conv_2, act_type = "tanh")
+tanh_2 <- mx.symbol.Activation(data = conv_2, act_type = "relu")
 pool_2 <- mx.symbol.Pooling(data = tanh_2, pool_type = "max", kernel = c(2,2), stride = c(2,2))
 
 # 1st fully connected layer
 flat <- mx.symbol.Flatten(data = pool_2)
 fcl_1 <- mx.symbol.FullyConnected(data = flat, num_hidden = 500)
-tanh_3 <- mx.symbol.Activation(data = fcl_1, act_type = "tanh")
+tanh_3 <- mx.symbol.Activation(data = fcl_1, act_type = "relu")
 
 # 2nd fully connected layer
 fcl_2 <- mx.symbol.FullyConnected(data = tanh_3, num_hidden = 2)
